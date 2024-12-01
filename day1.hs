9f4844734adf978b3881e21a2b3ef2c623ecb035
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2024-12-01 15:59:00 +1000

    chore: simplify day1

--- day1.hs
+++ day1.hs
@@ -1,15 +1,5 @@
 import Data.List (sort)
 
-getTotalDistance :: [Int] -> [Int] -> Int
-getTotalDistance [] [] = 0
-getTotalDistance [] _ = 0
-getTotalDistance _ [] = 0
-getTotalDistance (x : xs) (y : ys) = abs (x - y) + getTotalDistance xs ys
-
-getScore :: [Int] -> Int -> Int
-getScore [] _ = 0
-getScore ys x = (length . filter (== x) $ ys) * x
-
 main :: IO ()
 main = do
   input <- readFile "day1_input.txt"
@@ -19,9 +9,9 @@ main = do
   let ys = map last inputNumbers
 
   -- part 1
-  print $ getTotalDistance (sort xs) (sort ys)
+  print $ sum $ zipWith (\x y -> abs (x - y)) (sort xs) (sort ys)
 
   -- part 2
-  print $ sum $ map (getScore ys) xs
+  print $ foldl (\acc x -> acc + (x * (length . filter (== x) $ ys))) 0 xs
 
   return ()
